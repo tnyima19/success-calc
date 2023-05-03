7e019a7fa723796b7cb1f163d17e304b3630d589
tnyima19 <66368294+tnyima19@users.noreply.github.com>
2023-05-03 00:12:04 -0400

    slight changes

--- src/InitialFindings.js
+++ src/InitialFindings.js
@@ -49,7 +49,7 @@ function InitialFindings(){
         
         <h3>Types of Business</h3>
         <div class="row gx-1 gy-5">
-            <div class="col">
+            <div class="box">
                 <p>I also found out many types of business. There were more than types of business so I decided to focus on 
                 small business that interested me. Which are visualized below. Stoop Line Stand are groceries, and I also
                 found out that many Tobacco Retail Dealer also sell and advertise themselves as Groceries so that they can 
@@ -58,11 +58,10 @@ function InitialFindings(){
                 in contrast to shops that only sell alcohol or Electronic cigerettes.
             </p>
             </div>
-            <div class="col">
-                <div class="card-body">
-                    <img src={success_types} alt="success_types"></img>
-                </div>
+            <div class="card-body">
+                <img src={success_types} alt="success_types"></img>
             </div>
+            
         </div>
         <div class="container-fluid border">
             <div class="row gx-1 gy-5">
